ebc8925422e2f046d253d03ab6c55fce6c7a528f
vasiljevdenis <vasiljevdenis@bk.ru>
2023-02-27 00:48:07 +0300

    1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+import 'akar-icons-fonts';
+
+const bsOffcanvas = new bootstrap.Offcanvas('#offcanvasNavbar');
+const openCanvas = document.querySelector('header .navbar-toggler');
+
+openCanvas.addEventListener('click', function () {
+    bsOffcanvas.show();
+});
